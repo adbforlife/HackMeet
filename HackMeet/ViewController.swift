56133d50056ec4b85977f589d581bd22bf15379d
Albert Gao <adbforlife2018@gmail.com>
2017-05-20 10:19:33 -0400

    Initial Commit

---
+++ HackMeet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackMeet
+//
+//  Created by ADB on 5/20/17.
+//  Copyright © 2017 ADB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
