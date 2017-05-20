f884027ca5994ed1a5b5dab0b66932f8f45b7718
Asher Dale <asher@dales.org>
2017-05-20 12:26:27 -0400

    Made TabController

---
+++ HackMeet/SearchController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HackMeet
+//
+//  Created by ADB on 5/20/17.
+//  Copyright © 2017 ADB. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
